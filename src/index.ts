b5cede5afc9af1ffbdf4a3073b915a37cc37ba5d
vinayaktyagi <tyagi.vinayak@gmail.com>
2021-11-11 11:50:48 +0530

    First basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello Typescript");
+import http from 'http';
+import path from 'path';
+import fs from 'fs/promises';
+
+async function requestListener(req: http.IncomingMessage,res: http.ServerResponse){
+    const filePath = path.join(__dirname, 'static/index.html');
+    const data =await fs.readFile(filePath);
+    res.writeHead(200,{
+        "Content-Type": "text/html",
+
+    });
+
+    res.write(data);
+    res.end();
+    
+}
+
+http.createServer(requestListener).listen(9000,()=>{
+    console.log('server is running at port 9000');
+});
